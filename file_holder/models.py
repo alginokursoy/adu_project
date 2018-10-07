a6790171eb17404d5aef5e66c02d0df6cf588739
alginokursoy <algin.okursoy@gmail.com>
2018-10-08 00:27:41 +0300

    düzenleme

--- file_holder/models.py
+++ file_holder/models.py
@@ -3,7 +3,8 @@ from django.db import models
 # Create your models here.
 class FileBank(models.Model):
     file=models.FileField(null=False, blank=False)
-    course_title=models.CharField(blank=True, null=True, max_length=100)
+    file_name=models.CharField(blank=True, null=True, max_length=100)
+    description=models.TextField(default='none')
 
     def __str__(self):
         return self.file.name
